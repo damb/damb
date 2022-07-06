1692ab5080110c0ff2ef0da999ef1275ee066555
Daniel Armbruster <daniel.armbruster@sed.ethz.ch>
2022-07-06 11:35:33 +0000

    MAINT: Provide installation script

---
+++ install.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+#set -x
+
+# bash
+if [[ -f $HOME/.bash_profile && ! -L $HOME/.bash_profile ]]
+then
+  mv $HOME/.bash_profile $HOME/.bash_profile.bak
+fi
+
+for c in runcom/.bash_profile
+do
+  if [[ ! -L  $HOME/$(basename $c) ]]
+  then
+    ln -s -t $HOME "$(realpath $c)"
+  fi
+done
+
+# bashrc
+if [[ -f $HOME/.bashrc && ! -L $HOME/.bashrc ]]
+then
+  mv $HOME/.bashrc $HOME/.bashrc.bak
+fi
+if [[ ! -L $HOME/.bashrc ]]
+then
+  ln -s -T $HOME/.bash_profile $HOME/.bashrc
+fi
+
+# vim
+for c in config/vim/.vimrc config/vim/.vim
+do
+  if [[ ! -L  $HOME/$(basename $c) ]]
+  then
+    ln -s -t $HOME "$(realpath $c)"
+  fi
+done
+
+# git
+for c in config/git/.gitconfig
+do
+  if [[ ! -L  $HOME/$(basename $c) ]]
+  then
+    ln -s -t $HOME "$(realpath $c)"
+  fi
+done
+
