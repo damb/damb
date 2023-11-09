75db0a685309bc19e702339dbf853b3b34cd3193
Daniel Armbruster <dani.armbruster@gmail.com>
2023-11-09 12:12:18 +0100

    Sort keymaps

--- config/nvim/.config/nvim/lua/damb/keymap.lua
+++ config/nvim/.config/nvim/lua/damb/keymap.lua
@@ -10,13 +10,6 @@ vim.g.maplocalleader = "\\"
 -- normal mode (=n) --
 keymap("n", "<Leader>e", ":Lex 30<CR>", opts)
 
--- command-t
-keymap("n", "<Leader>ff", "<Plug>(CommandT)")
-keymap("n", "<Leader>fb", "<Plug>(CommandTBuffer)")
-keymap("n", "<Leader>fj", "<Plug>(CommandTJump)")
-keymap("n", "<Leader>fg", "<Plug>(CommandTGit)")
-keymap("n", "<Leader>fr", "<Plug>(CommandTRipgrep)")
-
 -- search and replace
 keymap(
   "n",
@@ -24,6 +17,13 @@ keymap(
   ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/g<Left><Left>",
   { noremap = true }
 )
+
+-- command-t
+keymap("n", "<Leader>ff", "<Plug>(CommandT)", opts)
+keymap("n", "<Leader>fb", "<Plug>(CommandTBuffer)", opts)
+keymap("n", "<Leader>fj", "<Plug>(CommandTJump)", opts)
+keymap("n", "<Leader>fg", "<Plug>(CommandTGit)", opts)
+keymap("n", "<Leader>fr", "<Plug>(CommandTRipgrep)", opts)
 
 -- undotree
 keymap("n", "<Leader>u", ":UndotreeToggle<CR>", opts)
