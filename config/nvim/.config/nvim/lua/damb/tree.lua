36b0ab2da30671578300128470c2c4bbb8a1ef8f
Daniel Armbruster <dani.armbruster@gmail.com>
2024-01-15 16:17:59 +0100

    Fix formatting

--- config/nvim/.config/nvim/lua/damb/tree.lua
+++ config/nvim/.config/nvim/lua/damb/tree.lua
@@ -1,17 +1,17 @@
 local success, nvim_tree = pcall(require, "nvim-tree")
 if not success then
-	vim.notify("WARN: failed to load 'nvim-tree' plugin")
-	return
+  vim.notify("WARN: failed to load 'nvim-tree' plugin")
+  return
 end
 
 -- TODO(damb): display file icons. See: https://github.com/nvim-tree/nvim-tree.lua#requirements.
 
 nvim_tree.setup({
-	respect_buf_cwd = true,
-	update_focused_file = {
-		enable = true,
-	},
-	view = {
-		width = 40,
-	},
+  respect_buf_cwd = true,
+  update_focused_file = {
+    enable = true,
+  },
+  view = {
+    width = 40,
+  },
 })
