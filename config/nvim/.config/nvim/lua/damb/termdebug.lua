38583fb6e7571d5e844535331605eec7f6b9410d
Daniel Armbruster <dani.armbruster@gmail.com>
2024-03-10 14:39:24 +0100

    Set configuration straight away

--- config/nvim/.config/nvim/lua/damb/termdebug.lua
+++ config/nvim/.config/nvim/lua/damb/termdebug.lua
@@ -1,4 +1,4 @@
-vim.g.termdebug_config = {}
--- References:
---   - https://www.dannyadam.com/blog/2019/05/debugging-in-vim/
-vim.g["termdebug_config"] = { wide = 1 }
+vim.g.termdebug_config = {
+  -- https://www.dannyadam.com/blog/2019/05/debugging-in-vim/
+  wide = 1,
+}
